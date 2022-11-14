027f44ab185c7543b5e2ad72df1234719a718f3d
Dei01 <Bilguudei0@icloud.com>
2022-11-14 15:31:59 +0800

    1.3

---
+++ Lec12.py
@@ -0,0 +1,56 @@
+
+# LIBRARIES 
+
+# MASSIVE 
+import numpy as np
+import pandas as pd
+# a = np.array([1,2,3])
+# a
+# type(a)
+
+# print('hi')
+
+
+# a.ndim 
+
+# a.size 
+# np.arange(0,10)
+
+# np.arange(0,12,3)
+# np.arange(0,3,0.5)
+
+# np.linspace(0,10,5) # функцийн эхний 2 аргумент массивын эхнйи ба ёүүлийн элементүүд байх ба 3 дахь нь эдгээр утгыг хэдэн завсар хуваагыг заана.
+
+
+# a = np.arange(10, 19).reshape((3, 3))
+# a
+
+
+# arr = np.array([1,2, 3, 4, 5])
+# print(arr.dtype)
+# print('------------------------------------------------------------')
+
+# a = np.arange(10, 15)
+# for i in a:
+# print (i)
+# print('------------------------------------------------------------')
+
+# b =np.arange(10, 19).reshape((3, 3))
+# for row in b:
+# print(row)
+
+# print('------------------------------------------------------------')
+
+# import numpy as np 
+# x = np.array([3, 5])
+# y = np.array([2, 5])
+# print(np.greater(x, y))
+
+
+obj = pd.Series([4,7,-5,3,])
+
+print(obj)
+print(obj.values)
+print(obj.index)
+
+
