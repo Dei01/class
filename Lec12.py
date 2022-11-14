f5ca0ebdab4197a8af294b38a19a54461d823627
Dei01 <Bilguudei0@icloud.com>
2022-11-14 16:06:19 +0800

    1.4

--- Lec12.py
+++ Lec12.py
@@ -52,5 +52,43 @@ obj = pd.Series([4,7,-5,3,])
 print(obj)
 print(obj.values)
 print(obj.index)
+print('------------------------------------------------------------')
 
 
+obj2 = pd.Series([4, 7, -5], index = ['a', 'b', 'c'])
+print(obj2)
+print(obj2/2)
+print(obj2+5)
+
+mydict = {'red':2000, 'blue':1000, 'yellow':500, 'orange':1000}
+colors = ['red', 'yellow', 'orange', 'blue', 'green']
+myseries = pd.Series(mydict, index = colors)
+myseries
+
+print('--------------------------ДАТАФРЭЙМИЙГ ҮҮСГЭХ----------------------------------')
+
+
+# ДАТАФРЭЙМИЙГ ҮҮСГЭХ
+
+data = {'color' : ['blue', 'green', 'yellow', 'red', 'white'], 'object' : ['ball', 'pen', 'pencil', 'paper', 'mug'], 'price' : [1.2, 1.0, 0.6, 0.9, 1.7]}
+frame = pd.DataFrame(data)
+print(frame)
+print('--------------------------frame1----------------------------------')
+
+frame1 = pd.DataFrame(data, columns = ['object', 'price'])
+print(frame1)
+print('-------------------------frame2-----------------------------------')
+
+frame2 = pd.DataFrame(data,
+index = ['one', 'two', 'three', 'four', 'five'])
+print(frame2)
+
+print('------------------------frame3------------------------------------')
+
+ #COMBINING NUMPY & PANDAS 
+ 
+frame3 = pd.DataFrame(np.arange(16).reshape((4, 4)), index = ['red', 'blue', 'yellow', 'white'],
+columns = ['ball', 'pen', 'pencil', 'paper']) 
+print(frame3)
+
+print('-------------------------frame4-----------------------------------')
